aab381aaa26f1a089d75f051a2cf5ebba410004f
Chae-Yeun Park <chae.yeun.park@gmail.com>
2025-02-23 20:11:45 +0900

    Fix all stuffs besides encode

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import pennylane as qml
 import numpy as np
+import jax
+import jax.numpy as jnp
+import optax
 from gates import Spin_2, Spin_3, create_singlet
-from generation import generate_sphere_point_cloud, generate_torus_point_cloud
 import pandas as pd
 import math
 import pennylane.numpy as pnp
 import itertools
 
-dev = qml.device("lightning.qubit", wires=18)  
+jax.config.update("jax_enable_x64", True)
 
+dev = qml.device("default.qubit", wires=18)  
+key = jax.random.key(1557)
 
 def loss(num_qubit, H, answer_train, index): 
-
     H_sigmoid = 1 / (1 + pnp.exp(-H))
     cross_entropy = 0
-    print(H)
-    print(H_sigmoid)
 
     for i  in range (num_qubit * index, num_qubit * (index + 1)):
         cross_entropy += answer_train[i] * pnp.log(H_sigmoid) + (1 - answer_train[i]) * pnp.log(1 - H_sigmoid)
@@ -25,8 +26,7 @@ def loss(num_qubit, H, answer_train, index):
 
     return cross_entropy
 
-
-def encode(num_wire, vector, Theta):
+def encode(num_wire, vector):
 
     # theta = Alpha * np.arccos(vector[2])
     # phi = Beta * np.arctan2(vector[1], vector[0])
@@ -71,27 +71,25 @@ def encode(num_wire, vector, Theta):
 
 
 def create_Hamiltonian(num_qubit):
-
-    H = sum([qml.PauliZ(i) @ qml.PauliZ((i + 1) % num_qubit) for i in range(num_qubit)])
-    H += sum([qml.PauliX(i) @ qml.PauliX((i + 1) % num_qubit) for i in range(num_qubit)])
-    H += sum([qml.PauliY(i) @ qml.PauliY((i + 1) % num_qubit) for i in range(num_qubit)])
+    terms = []
+    for i in range(num_qubit-1):
+        for j in range(i+1, num_qubit):
+            terms.append(qml.PauliX(i) @ qml.PauliX(j))
+            terms.append(qml.PauliY(i) @ qml.PauliY(j))
+            terms.append(qml.PauliZ(i) @ qml.PauliZ(j))
     
-    return H
+    return sum(terms)
 
 
 def prepare_init_state(num_qubit):
     for i in range(0, num_qubit, 2):
         create_singlet(i, i+1) 
 
-
-def create_u2_circuit(num_qubit, num_blocks, H, Theta, data, index):
-
-    def circuit_2qubits(params):
+def create_u2_circuit(num_qubit, num_blocks, H, Theta):
+    def circuit_2qubits(params, data_pt):
         prepare_init_state(num_qubit)
-        for i, vector in enumerate(data[num_qubit * index : num_qubit * (index + 1)]):
-            num_wire = i % num_qubit
-            if num_wire % 2 == 0:
-                encode(num_wire, vector, Theta)
+        for i, point in enumerate(data_pt):
+            encode(i, point / Theta)
 
         k = 0
         for l in range(num_blocks):
@@ -131,88 +129,44 @@ def create_u3_circuit(num_qubit, num_blocks, H, Theta, data, index):
 
     return circuit_3qubits
 
+def train(gate_type, dataset, minibatch_size, Theta, epochs, key, **adam_opt):
+    train_dataset_x = dataset['train_dataset_x']
+    train_dataset_y = dataset['train_dataset_y']
+    test_dataset_x = dataset['test_dataset_x']
+    test_dataset_y = dataset['test_dataset_y']
 
-def create_data(outer_radius, inner_radius, sphere_radius, num_vectors):
+    assert len(train_dataset_x) == len(train_dataset_y)
+    assert len(train_dataset_x) % minibatch_size == 0
+    batch_size = len(train_dataset_x)
 
-    sphere_df = generate_sphere_point_cloud(max_radius=sphere_radius, num_vectors=num_vectors)
-    torus_df = generate_torus_point_cloud(inner_radius=inner_radius, outer_radius=outer_radius, num_vectors=num_vectors)
+    train_dataset_x = train_dataset_x.reshape(batch_size // minibatch_size, minibatch_size, -1, 3)
+    train_dataset_y = train_dataset_y.reshape(batch_size // minibatch_size, minibatch_size)
 
-    sphere_vector_train = []
-    torus_vector_train = []
-    sphere_vector_answer = []
-    torus_vector_answer = []
+    ham = create_Hamiltonian(num_qubit)
+    ham_sparse = qml.SparseHamiltonian(ham.sparse_matrix(), wires=range(18))
 
-    for i in range(num_vectors):
-
-        sphere_vector = sphere_df.iloc[i, :-1].values
-        sphere_vector = sphere_vector / np.linalg.norm(sphere_vector) 
-        sphere_vector_train.append(sphere_vector)
-        sphere_vector_answer.append(sphere_df.iloc[i, -1])
-
-        torus_vector = torus_df.iloc[i, :-1].values
-        torus_vector = torus_vector / np.linalg.norm(torus_vector)
-        torus_vector_train.append(torus_vector)
-        torus_vector_answer.append(torus_df.iloc[i, -1])
-
-
-    num_train = int(0.75 * num_vectors) 
-    num_val = int(0.25 * num_vectors)
-
-    feats_train_sphere = [sphere_vector_train[i] for i in range(num_train)]
-    answer_train_sphere = [sphere_vector_answer[i] for i in range(num_train)]
-    feats_val_sphere = [sphere_vector_train[i] for i in range(num_train, num_vectors)]
-    answer_val_sphere = [sphere_vector_answer[i] for i in range(num_train, num_vectors)]
-
-    feats_train_torus = [torus_vector_train[i] for i in range(num_train)]
-    answer_train_torus = [torus_vector_answer[i] for i in range(num_train)]
-    feats_val_torus = [torus_vector_train[i] for i in range(num_train, num_vectors)]
-    answer_val_torus = [torus_vector_answer[i] for i in range(num_train, num_vectors)]
-    
-    feats_train = feats_train_sphere + feats_train_torus
-    answer_train = answer_train_sphere + answer_train_torus
-    feats_val = feats_val_sphere + feats_val_torus
-    answer_val = answer_val_sphere + answer_val_torus
-    array_1 = []
-    array_2 = []
-    array_3 = []
-    array_4 = []
-
-    np.random.seed(0)
-
-    index_permutation_train = np.random.permutation(int(2 * num_train / num_qubit))
-    for i in range(int (2 * num_train / num_qubit)):
-        array_1.append(feats_train[num_qubit * index_permutation_train[i] : num_qubit * (index_permutation_train[i] + 1)])
-        array_2.append(answer_train[num_qubit * index_permutation_train[i] : num_qubit * (index_permutation_train[i] + 1)])
-    feats_train = list(itertools.chain.from_iterable(array_1))
-    answer_train = list(itertools.chain.from_iterable(array_2))
-
-    index_permutation_val = np.random.permutation(int(2 * num_val / num_qubit))
-    for i in range(int (2 * num_val / num_qubit)):
-        array_3.append(feats_val[num_qubit * index_permutation_val[i] : num_qubit * (index_permutation_val[i] + 1)])
-        array_4.append(answer_val[num_qubit * index_permutation_val[i] : num_qubit * (index_permutation_val[i] + 1)])
-    feats_val = list(itertools.chain.from_iterable(array_3))
-    answer_val = list(itertools.chain.from_iterable(array_4))
-
-    return feats_train, answer_train, feats_val, answer_val
-
-
-def train(gate_type):
-
-    if (gate_type) == ("u2"):
+    if gate_type == "u2":
         init_u2 = init_scale * math.pi/(2 * num_qubit * num_blocks)
-        params_u2 = init_u2 * pnp.random.rand(2 * num_qubit * num_blocks)
-        for index in range(batch_train):
-            circuit_u2 = qml.QNode(create_u2_circuit(num_qubit, num_blocks, ham_sparse, Theta, feats_train, index), dev, diff_method="parameter-shift")
-            
-            for epoch in range(epochs):
-                params_u2, cost = opt_u2.step_and_cost(lambda p: loss(num_qubit, circuit_u2(p), answer_train, index), params_u2)
-                print(f"{epoch}\t{cost}")
-                print(params_u2)
+        params = init_u2 * jax.random.uniform(key, (2 * num_qubit * num_blocks,))
 
-        params_train = params_u2
-        print(np.array(params_train))
-        return params_train
+        circuit = qml.QNode(create_u2_circuit(num_qubit, num_blocks, ham_sparse, Theta), device = dev, interface='jax')
 
+        logits = circuit(params, train_dataset_x[0])
+
+        def loss_fn(params, mini_batch_x, mini_batch_y):
+            logits = circuit(params, mini_batch_x)
+            return optax.losses.sigmoid_binary_cross_entropy(logits, mini_batch_y)
+
+        solver = optax.adam(**adam_opt)
+        for epoch in range(epochs):
+            for i in range(batch_size // minibatch_size):
+                grad = jax.grad(loss_fn, argnums=0)(params, train_dataset_x[i], train_dataset_y[i])
+                updates, opt_state = solver.update(grad, opt_state, params)
+                params = optax.apply_updates(params, updates)
+            test_error = jnp.mean((circuit(params, test_dataset_x) > 0) == test_dataset_y)
+            print(f"{epoch}, {test_error}")
+
+    """
     if (gate_type) == ("u3"):
         init_u3 = init_scale*math.pi/(4 * num_qubit * num_blocks)
         params_u3 = init_u3 * pnp.random.rand(4 * num_qubit * num_blocks)
@@ -230,67 +184,18 @@ def train(gate_type):
 
     else:
         print("Type Error")
+    """
 
 
-def accuracy(labels, predictions):
-    acc = sum(abs(l - p) < 0.5 for l, p in zip(labels, predictions))
-    acc = acc / len(labels)
-    return acc
+# Load dataset
+dataset = np.load('dataset.npz')
 
-
-def predict(gate_type, params_train):
-
-    if (gate_type) == ("u2"):
-        predictions = []
-        for index in range(batch_pre):
-            circuit_u2 = qml.QNode(create_u2_circuit(num_qubit, num_blocks, ham_sparse, Theta, feats_val, index), dev)
-            pred = circuit_u2(params_train)
-            pred_sigmoid = 1 / (1 + np.exp(-pred))
-            for  i in range(num_qubit):
-                predictions.append(float(pred_sigmoid))
-
-        acc = accuracy(answer_val, predictions)
-        print(acc)
-
-    if (gate_type) == ("u3"):
-        predictions = []        
-        for index in range(batch_pre):
-            circuit_u3 = qml.QNode(create_u3_circuit(num_qubit, num_blocks, ham_sparse, Theta, feats_val, index), dev)
-            pred = circuit_u3(params_train)
-            pred_sigmoid = 1 / (1 + np.exp(-pred))  
-            predictions.append(float(pred_sigmoid)) 
-            for  i in range(num_qubit):
-                predictions.append(float(pred_sigmoid))
-
-        acc = accuracy(answer_val, predictions)
-        print(acc)
-
-
-
-inner_radius = 3
-outer_radius = 5
-num_vectors = 48
-sphere_radius = inner_radius + outer_radius
 num_qubit = 18
-ham = create_Hamiltonian(18)
-ham_sparse = qml.SparseHamiltonian(ham.sparse_matrix(), wires=range(18))
-Theta = np.pi/4
+Theta = 8.0
 
-
-batch_train = int((2 * num_vectors * 0.75 / num_qubit) - 1)
-batch_pre = int((2 * num_vectors - num_qubit * (batch_train + 1)) / num_qubit)
-adam_step = 0.05
 num_blocks = 4
 init_scale = 1
 epochs = 10
-opt_u2 = qml.AdamOptimizer(stepsize=adam_step)
-opt_u3 = qml.AdamOptimizer(stepsize=adam_step)
 
-
-feats_train, answer_train, feats_val, answer_val = create_data(outer_radius, inner_radius, sphere_radius, num_vectors) 
-
-gate_type = input("Write gate_type")
-
-params_train = train(gate_type)
-print(params_train)
-predict(gate_type ,params_train)
+key, key_r = jax.random.split(key)
+train("u2", dataset, 8, Theta, epochs, key_r, learning_rate = 0.03)
