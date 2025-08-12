fd595f059956f2bef881e7e6dbd19b036e047915
semin-park23 <2023193003@yonsei.ac.kr>
2025-08-13 01:34:28 +0900

    Add files via upload

--- gates_twirling_perm.py
+++ gates_twirling_perm.py
@@ -19,14 +19,14 @@ class Spin_twirling(qml.operation.Operation):
     
         eigenvals_1, U_1 = eigh(Gate_1)
         
-        max_eigenval_1 = jnp.max(jnp.abs(eigenvals_1))
-        normalized_eigenvals_1 = eigenvals_1 / max_eigenval_1
+        # max_eigenval_1 = jnp.max(jnp.abs(eigenvals_1))
+        # normalized_eigenvals_1 = eigenvals_1 / max_eigenval_1
         
-        normalized_eigenvals_1 = jnp.where(
-            jnp.abs(normalized_eigenvals_1) < 1e-13,
-            0.0,
-            normalized_eigenvals_1
-        )
+        # normalized_eigenvals_1 = jnp.where(
+        #     jnp.abs(normalized_eigenvals_1) < 1e-13,
+        #     0.0,
+        #     normalized_eigenvals_1
+        # )
 
         filename = f"perm_matrix_{num_qubit}_{num_perm}_minus.npy"
         matrix_2 = np.load(filename)
@@ -34,20 +34,20 @@ class Spin_twirling(qml.operation.Operation):
     
         eigenvals_2, U_2 = eigh(Gate_2)
         
-        max_eigenval_2 = jnp.max(jnp.abs(eigenvals_2))
-        normalized_eigenvals_2 = eigenvals_2 / max_eigenval_2
+        # max_eigenval_2 = jnp.max(jnp.abs(eigenvals_2))
+        # normalized_eigenvals_2 = eigenvals_2 / max_eigenval_2
         
-        normalized_eigenvals_2 = jnp.where(
-            jnp.abs(normalized_eigenvals_2) < 1e-13,
-            0.0,
-            normalized_eigenvals_2
-        )
+        # normalized_eigenvals_2 = jnp.where(
+        #     jnp.abs(normalized_eigenvals_2) < 1e-13,
+        #     0.0,
+        #     normalized_eigenvals_2
+        # )
         
         return [
-            qml.QubitUnitary(U_1.conj().T, wires=range(num_qubit)),
-            qml.DiagonalQubitUnitary(jnp.exp(1j * theta0 * normalized_eigenvals_1), wires=range(num_qubit)),
-            qml.QubitUnitary(U_1, wires=range(num_qubit)),
-            qml.QubitUnitary(U_2.conj().T, wires=range(num_qubit)),
-            qml.DiagonalQubitUnitary(jnp.exp(1j * theta1 * normalized_eigenvals_2), wires=range(num_qubit)),
-            qml.QubitUnitary(U_2, wires=range(num_qubit))
+            qml.QubitUnitary(U_1.conj().T, wires=wires),
+            qml.DiagonalQubitUnitary(jnp.exp(1j * theta0 * eigenvals_1), wires=wires),
+            qml.QubitUnitary(U_1, wires=wires),
+            qml.QubitUnitary(U_2.conj().T, wires=wires),
+            qml.DiagonalQubitUnitary(jnp.exp(1j * theta1 * eigenvals_2), wires=wires),
+            qml.QubitUnitary(U_2, wires=wires)
         ]
