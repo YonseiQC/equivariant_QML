80f5efda66d9f60ff5f5af842eaeadfac7a49db7
Semin Park <2023193003@yonsei.ac.kr>
2025-12-23 12:15:17 +0000

    Update scrip

--- run_on_pod.sh
+++ run_on_pod.sh
@@ -1,3 +1,7 @@
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
 SEEDS=(1557 831 121 2023 2024 2025 2026)
 
 for index in "${!SEEDS[@]}"; do
@@ -6,3 +10,5 @@ for index in "${!SEEDS[@]}"; do
     CUDA_VISIBLE_DEVICES="$index" python3 HyQuRP_light_modelnet,shapenet.py $seed > "modelnet_shapenet_$seed.dat" 2> modelnet_shapenet_$seed.err &
 done
 wait
+
+cp modelnet_shapenet_*.dat /workspace
